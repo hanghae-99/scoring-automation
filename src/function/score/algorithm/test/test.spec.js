c08b3309f0246bd08167687dfda18bac274b15b4
admin <js.byeon@teamsparta.co>
2023-06-06 01:46:11 +0900

    add test case

--- src/function/score/algorithm/test/test.spec.js
+++ src/function/score/algorithm/test/test.spec.js
@@ -3,12 +3,24 @@ import { readFileSync } from "node:fs";
 import { strict as assert } from "node:assert";
 import { test } from "node:test";
 
-const argsArr = [["world"], ["JS"], ["HANGHAE99"]];
-const answers = ["hello, world!", "hello, JS!", "hello, HANGHAE99!"];
-const code = readFileSync(new URL("example.js", import.meta.url), "utf8");
+// node --test
+test("100점이 나와야합니다", () => {
+  const argsArr = [["world"], ["JS"], ["HANGHAE99"]];
+  const answers = ["hello, world!", "hello, JS!", "hello, HANGHAE99!"];
+  const code = readFileSync(new URL("example.js", import.meta.url), "utf8");
 
-// node src/function/score/algorithm/test/test.spec.js
-
-test("score example", () => {
   assert.equal(score("javascript", code, argsArr, answers), 100);
+});
+
+test("75점이 나와야합니다", () => {
+  const argsArr = [["world"], ["JS"], ["HANGHAE99"], ["Bootcamp"]];
+  const answers = [
+    "goodbye, world!",
+    "hello, JS!",
+    "hello, HANGHAE99!",
+    "hello, Bootcamp!",
+  ];
+  const code = readFileSync(new URL("example.js", import.meta.url), "utf8");
+
+  assert.equal(score("javascript", code, argsArr, answers), 75);
 });
