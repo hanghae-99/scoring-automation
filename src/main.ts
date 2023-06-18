8da4666284a51fb61047be535c3cfa9291c89737
BJS-kr <hahajeng1234@gmail.com>
2023-06-18 22:43:02 +0900

    add comment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ScoreModule } from './cli.module';
-
+// ts-node src/main.ts -f 답안_예시.xlsx -s 시트1 -t alg
 async function bootstrap() {
   await NestFactory.createApplicationContext(ScoreModule);
 }
