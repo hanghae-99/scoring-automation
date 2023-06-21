a2e6f5663227c4e1cf3f6f2649b96163dc9e9c0c
admin <js.byeon@teamsparta.co>
2023-06-21 16:24:30 +0900

    complete js

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.';
+// npm i -g ts-node
 // ts-node src/main.ts -f 답안_예시.xlsx -s 자동채점테스트 -t alg
 async function bootstrap() {
   await NestFactory.createApplicationContext(AppModule);
