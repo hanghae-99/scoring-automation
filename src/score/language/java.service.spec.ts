2277a24436dd910065470b5feebeae27b728305b
jhyeon.kim <jhyeon.kim@teamsparta.co>
2023-10-24 14:07:21 +0900

    add: 제출한 코드에 대해 테스트케이스와 기대한 결과 비교 코드

--- src/score/language/java.service.spec.ts
+++ src/score/language/java.service.spec.ts
@@ -1,61 +1,82 @@
 import { JavaService } from './java.service';
 
 describe('JavaService', () => {
-  const service = new JavaService();
-
-  it('should execute Java code correctly', () => {
-    const userCode = `
-      public class UserSolution {
-        public static void main(String[] args) {
-          System.out.println("\\"" + args[0] + "\\"");
-        }
-      }
-    `;
-    const argsArr = [['Hello'], ['World']];
-    const result = service.executeJAVAOnEachArgs(userCode, argsArr);
-    expect(result).toEqual(['Hello', 'World']);
-  });
-});
-
-describe('UserSolution', () => {
   const javaService = new JavaService();
 
-  it('should return correct array based on signs', () => {
-    const userCode = `import java.util.Arrays;
+  it('1번 문제 - 덧셈과 부호변화 로직에 대한 코드를 올바르게 실행할 수 있다.', () => {
+    const AlgorithmType = 'MATRIX_OPERATION';
+    const userCode = `public class UserSolution {
 
-public class test1 {
-    public int[][] solution(int[][] arr1, int[][] arr2, boolean[][] signs) {
-        int[][] answer = new int[arr1.length][arr1[0].length];
-        for(int i = 0; i < arr1.length; i++){
-            for(int j = 0; j < arr1[i].length ; j++){
-                if(signs[i][j]){
-                    answer[i][j] = arr1[i][j] + arr2[i][j];
-                } else {
-                    answer[i][j] = (arr1[i][j] + arr2[i][j]) * -1;
-                }
+    public static int[][] solution(int[][] arr1, int[][] arr2, boolean[][] signs) {
+        int rows = arr1.length;
+        int cols = arr1[0].length;
+        int[][] answer = new int[rows][cols];
 
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int val1 = signs[i][j] ? arr1[i][j] : -arr1[i][j];
+                int val2 = signs[i][j] ? arr2[i][j] : -arr2[i][j];
+                answer[i][j] = val1 + val2;
             }
         }
 
         return answer;
     }
-
-    public static void main(String[] args) {
-        test1 method = new test1();
-        int[][] arr1 = {{5,7,1},{2,3,5}};
-        int[][] arr2 = {{5,1,6},{7,5,6}};
-        boolean[][] signs={{true,true,false},{false,true,false}};
-        System.out.println(Arrays.deepToString(method.solution(arr1, arr2, signs)));
-    }
 }`;
 
-    const argsArr = [[1], [2], [3]]; // 이게 사실 필요가 없는 느낌인데
+    const argsArr = [
+      [
+        [
+          [1, 2],
+          [2, 3],
+        ],
+        [
+          [3, 4],
+          [5, 6],
+        ],
+        [
+          [true, true],
+          [true, false],
+        ],
+      ],
+      [
+        [[1], [2]],
+        [[3], [4]],
+        [[false], [false]],
+      ],
+      [
+        [
+          [5, 7, 1],
+          [2, 3, 5],
+        ],
+        [
+          [5, 1, 6],
+          [7, 5, 6],
+        ],
+        [
+          [true, true, false],
+          [false, true, false],
+        ],
+      ],
+    ];
     const expectedOutput = [
-      [10, 8, -7],
-      [-9, 8, -11],
+      [
+        [4, 6],
+        [7, -9],
+      ],
+      [[-4], [-6]],
+      [
+        [10, 8, -7],
+        [-9, 8, -11],
+      ],
     ];
 
-    const output = javaService.executeJAVAOnEachArgs(userCode, argsArr);
-    expect(output[0]).toEqual(expectedOutput);
+    const output = javaService.executeJAVAOnEachArgs(
+      userCode,
+      argsArr,
+      AlgorithmType,
+    );
+    console.log(`**** output: ${JSON.stringify(output)}`);
+    expect(output).toEqual(expectedOutput);
   });
 });
--- src/score/language/java.service.ts
+++ src/score/language/java.service.ts
@@ -1,37 +1,157 @@
-import { execSync } from 'child_process';
+import { execSync, spawnSync } from 'child_process';
 import { writeFileSync, unlinkSync } from 'fs';
+import { join, dirname } from 'path';
+type SimpleType = number | string | boolean;
+type ArgumentType = SimpleType[] | SimpleType[][];
 
 export class JavaService {
-  public executeJAVAOnEachArgs(userCode: string, argsArr: any[][]): any[] {
-    const tempSrcFile = `UserSolution.java`; // 임시 소스 파일 이름
+  private static MATRIX_OPERATION_TEMPLATE = `
+import java.util.*;
+import com.google.gson.Gson;
 
-    // 사용자의 메인 클래스 이름을 가져옵니다.
-    const classNameMatch = userCode.match(/public class (\w+)/);
-    if (!classNameMatch) {
-      console.error('유효한 Java 클래스를 찾을 수 없습니다.');
-      return Array(argsArr.length).fill(null);
+public class UserSolution {
+    PLACEHOLDER
+
+    public static void main(String[] args) {
+        // Deserialize the JSON input
+        Gson gson = new Gson();
+        List<List<List<Object>>> input = gson.fromJson(args[0], List.class);
+
+        int[][] arr1 = convertToInt2DArray(input.get(0));
+        int[][] arr2 = convertToInt2DArray(input.get(1));
+        boolean[][] signs = convertToBoolean2DArray(input.get(2));
+
+        int[][] result = solution(arr1, arr2, signs);
+        System.out.print(Arrays.deepToString(result));
     }
 
-    const originalClassName = classNameMatch[1];
-    // 원래 클래스 이름을 UserSolution으로 모두 교체합니다.
-    userCode = userCode.replace(
-      new RegExp(originalClassName, 'g'),
-      'UserSolution',
+    private static int[][] convertToInt2DArray(List<List<Object>> input) {
+        int[][] output = new int[input.size()][];
+        for (int i = 0; i < input.size(); i++) {
+            output[i] = input.get(i).stream().mapToInt(num -> ((Double) num).intValue()).toArray();
+        }
+        return output;
+    }
+
+    private static boolean[][] convertToBoolean2DArray(List<List<Object>> input) {
+        boolean[][] output = new boolean[input.size()][];
+        for (int i = 0; i < input.size(); i++) {
+            List<Object> row = input.get(i);
+            boolean[] boolRow = new boolean[row.size()];
+            for (int j = 0; j < row.size(); j++) {
+                boolRow[j] = (Boolean) row.get(j);
+            }
+            output[i] = boolRow;
+        }
+    return output;
+}
+
+}
+`;
+
+  private static extractSolutionMethod(userCode: string): string {
+    const jarPath = join(
+      dirname(__filename),
+      'libs',
+      'MethodExtractor-1.0-SNAPSHOT.jar',
     );
-    // 수정된 사용자 코드로 임시 Java 파일을 생성합니다.
-    writeFileSync(tempSrcFile, userCode, 'utf-8');
+    const command = `echo "${userCode}" | java -jar ${jarPath}`;
+    const output = execSync(command, { encoding: 'utf-8' }).trim();
+    console.log(`output from extractSolutionMethod: ${output})`);
+    if (!output) {
+      throw new Error('Failed to extract the solution method.');
+    }
+
+    return output;
+  }
+
+  private static integrateUserCodeWithTemplate(
+    userCode: string,
+    templateType: string,
+  ): string {
+    const solutionMethodBody = this.extractSolutionMethod(userCode);
+
+    let template = '';
+    switch (templateType) {
+      case 'MATRIX_OPERATION':
+        template = this.MATRIX_OPERATION_TEMPLATE;
+        break;
+      default:
+        throw new Error('유효하지 않은 템플릿 유형입니다.');
+    }
+
+    return template.replace('PLACEHOLDER', solutionMethodBody);
+  }
+
+  // private static integrateUserCodeWithTemplate(
+  //   userCode: string,
+  //   templateType: string,
+  // ): string {
+  //   console.log(`(1) userCode: ${userCode}`);
+  //   const solutionMethodBodyMatch = userCode.match(
+  //     /public static int\[\]\[\] solution\(.+\) \{([\s\S]+?)\}/,
+  //   );
+  //   console.log(`(2) solutionMethodBodyMatch: ${solutionMethodBodyMatch}`);
+  //   const solutionMethodMatch = solutionMethodBodyMatch[1];
+  //   console.log(`(3) solutionMethodMatch: ${solutionMethodMatch}`);
+  //
+  //   if (!solutionMethodMatch) {
+  //     throw new Error('유효한 solution 메서드를 찾을 수 없습니다.');
+  //   }
+  //   let template = '';
+  //   switch (templateType) {
+  //     case 'MATRIX_OPERATION':
+  //       template = this.MATRIX_OPERATION_TEMPLATE;
+  //       break;
+  //     default:
+  //       throw new Error('유효하지 않은 템플릿 유형입니다.');
+  //   }
+  //
+  //   return template.replace('PLACEHOLDER', solutionMethodMatch);
+  // }
+
+  public executeJAVAOnEachArgs(
+    userCode: string,
+    argsArr: ArgumentType[][],
+    templateType: string,
+  ): any[] {
+    console.log(`argsArr: ${JSON.stringify(argsArr)}`);
+
+    const integratedCode = JavaService.integrateUserCodeWithTemplate(
+      userCode,
+      templateType,
+    );
+    console.log('✅ Integrated Java Code:\n', integratedCode);
+    const tempSrcFile = 'UserSolution.java';
+    writeFileSync(tempSrcFile, integratedCode, 'utf-8');
 
     try {
-      // 임시 Java 파일을 컴파일합니다.
-      execSync(`javac ${tempSrcFile}`);
-
-      // 각 테스트 케이스에 대해 Java 클래스를 실행합니다.
+      // execSync(`javac ${tempSrcFile}`);
+      // execSync(`javac -cp ./libs/gson-2.8.8.jar ${tempSrcFile}`);
+      const jarPath = join(dirname(__filename), 'libs', 'gson-2.8.8.jar');
+      execSync(`javac -cp ${jarPath} UserSolution.java`);
       return argsArr.map((args, idx) => {
+        console.log(`${idx + 1}번째 테스트 케이스`);
+        console.log(args);
         try {
-          const output = execSync(`java UserSolution ${args.join(' ')}`, {
-            encoding: 'utf-8',
-          });
-          return JSON.parse(output); // 출력값을 JSON 형태로 변환 (출력값이 JSON 형태여야 함)
+          const jsonInput = JSON.stringify(args);
+          const jarPath = join(dirname(__filename), 'libs', 'gson-2.8.8.jar');
+
+          // Use spawnSync instead of execSync to better handle both stdout and stderr
+          const child = spawnSync(
+            'java',
+            ['-cp', `.:${jarPath}`, 'UserSolution', jsonInput],
+            {
+              encoding: 'utf-8',
+            },
+          );
+
+          const stdout = child.stdout.trim(); // This contains your actual result
+          const stderr = child.stderr.trim(); // This contains the logs from System.err.println
+
+          console.log(`Logs from Java App: ${stderr}`); // You can log the stderr output for debugging purposes
+
+          return JSON.parse(stdout);
         } catch (e: any) {
           console.error(`테스트 케이스 ${idx + 1}에서 오류 발생: ${e.message}`);
           return null;
@@ -41,9 +161,12 @@ export class JavaService {
       console.error(`컴파일 오류: ${e.message}`);
       return Array(argsArr.length).fill(null);
     } finally {
-      // 임시 파일들을 제거합니다.
-      unlinkSync(tempSrcFile);
-      unlinkSync('UserSolution.class');
+      unlinkSync('UserSolution.java');
+      try {
+        unlinkSync('UserSolution.class');
+      } catch (e) {
+        console.log('Failed to remove UserSolution.class');
+      }
     }
   }
 }
